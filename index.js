8144b300f86700f0db7442924cf6a9f0df2ccf4a
shayandutta <shayandutta23@gmail.com>
2025-02-13 21:09:09 +0530

    added template literals

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 //logical AND | logical OR
 
 
-function getName(name){
-    return name;
-}
-let a = true;
-let b = false;
+// function getName(name){
+//     return name;
+// }
+// let a = true;
+// let b = false;
 
-console.log(a&&getName('shayan'));
-console.log(a||getName('shayan')); 
+// console.log(a&&getName('shayan'));
+// console.log(a||getName('shayan')); 
+
+
+
+
+//template literals
+
+let name1= "shayan";
+let name2= "dutta";
+
+console.log(`${name1} ${name2}`);
