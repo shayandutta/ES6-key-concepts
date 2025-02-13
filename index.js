f58be2d499bfea5e8ca2cd9b6ee70098724ed7fa
shayandutta <shayandutta23@gmail.com>
2025-02-13 21:28:59 +0530

    added ternary operator

--- index.js
+++ index.js
@@ -15,7 +15,30 @@
 
 //template literals
 
-let name1= "shayan";
-let name2= "dutta";
+// let name1= "shayan";
+// let name2= "dutta";
 
-console.log(`${name1} ${name2}`);
+// console.log(name1 + " " + name2); //normal string concatenation
+// console.log(`${name1} ${name2}`); //template literals
+
+
+
+
+//Ternary Operators
+
+let getRecipe1 = false;
+
+function getRecipeOneName(recipeName){
+    console.log(recipeName) ;
+}
+function getRecipeTwoName(recipeName){
+    console.log(recipeName);
+}
+
+// if(getRecipe1){
+//     console.log("pizza")
+// }else{
+//     console.log("coke")
+// }
+
+getRecipe1 ? getRecipeOneName('pizza') : getRecipeTwoName('coke');
